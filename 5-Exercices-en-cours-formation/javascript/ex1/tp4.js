a5d4b43c3ce256af28ebd7c363dcffe785ed4d48
Sandy follana <follana.sandy@gmail.com>
2022-11-21 17:13:32 +0100

    horaires

--- 5-Exercices-en-cours-formation/javascript/ex1/tp4.js
+++ 5-Exercices-en-cours-formation/javascript/ex1/tp4.js
@@ -25,3 +25,40 @@ function ligne(){
 
 alert(`Votre IMC est de ${ligne()}`);
 
+const dayToday = new Date();
+
+alert("Nous sommes le" + " " + dayToday);
+
+function noonOrNot() {
+  let hour = dayToday.getHours();
+
+  console.log(typeof hour + hour);
+
+  if (hour < 12 && hour > 7) {
+    alert("C'est le matin");
+  } else if (hour >= 12 && hour < 20) {
+    alert("C'est l'après-midi");
+  } else {
+    alert("Arte c'est la nouit !");
+  }
+}
+
+noonOrNot();
+
+//Correction
+
+function timeOfDay(heure) {
+  if (heure >= 0 && heure < 24) {
+    if (heure < 12) {
+      console.log("c'est le matin");
+    } else if (heure < 18) {
+      console.log("c'est l'après-midi");
+    } else {
+      console.log("c'est la soirée");
+    }
+  } else {
+    console.log("rentrez une heure valable !");
+  }
+}
+
+timeOfDay(12.22);

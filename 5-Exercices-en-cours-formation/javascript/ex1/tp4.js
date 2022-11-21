0a4862c5bf4c3d5d4dc60410480d60ff1fb06e9f
Sandy follana <follana.sandy@gmail.com>
2022-11-21 17:55:29 +0100

    correctif

--- 5-Exercices-en-cours-formation/javascript/ex1/tp4.js
+++ 5-Exercices-en-cours-formation/javascript/ex1/tp4.js
@@ -1,4 +1,4 @@
-function anniversaire(age){
+/*function anniversaire(age){
 
     result=(age+1);
     alert("vous aurez bientôt"+" "+result+" "+"ans!");
@@ -62,3 +62,47 @@ function timeOfDay(heure) {
 }
 
 timeOfDay(12.22);
+
+let identify =prompt("Quel est votre prénom?") 
+
+switch (identify) {
+    case "joanna":
+      console.log("oh une fille!");
+      break;
+    case "marc":
+      console.log("tiens un garçon!");
+      break;
+    case "marie":
+      console.log("Encore une fille!");
+      break;
+    case "Dorian":
+      console.log("Et hop! un garçon");
+      break;
+    default:
+      console.log("Désolé,il y a eu méprise à la naissance ceci n'est point un humain");
+  }
+  
+
+  let surname = prompt("Quel est votre prénom");
+let men = "Tu est un gars";
+let woman = "Tu est une femme";
+let error = "Ceci n'est pas un prénom";
+switch (surname.toLowerCase()) {
+  case "jean":
+  case "bernard":
+  case "benjamin":
+    console.log(men);
+    break;
+  case "jeanne":
+  case "maria":
+  case "fatima":
+  case "ophelie":
+    console.log(woman);
+    break;
+  default:
+    console.log(error);
+    break;
+}
+age = prompt("Quel âge as tu ?");
+age >= 18 ? console.log("tu est majeur") : console.log("tu est mineur");
+  */

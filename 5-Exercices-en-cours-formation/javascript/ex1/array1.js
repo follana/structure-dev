39779c51218059b5d3da4137059c3f836bfddaed
Sandy follana <follana.sandy@gmail.com>
2022-11-22 17:45:50 +0100

    soir

---
+++ 5-Exercices-en-cours-formation/javascript/ex1/array1.js
@@ -0,0 +1,33 @@
+/*console.log("tableau");
+
+let tab =["nom",21[32,24,52], true];
+
+console.log(tab);
+console.log(tab[0]);
+
+console.table(tab);
+
+let tableau1=[["titi", "tata", "toto"],
+["jiji", "jaja", "jojo"],];
+
+tableau1.splice (2,0,["tabEx"]);
+
+console.table(tableau1);
+*/
+
+let formateur = ["Ophelie", "Benjamin", "Mathieu", "Leo"];
+
+let benjamin = {
+  nom: "Jully",
+  prenom: "Boy",
+  force: "extreme",
+  ego: "surdimensionné",
+};
+
+
+function read(){
+    console.table([formateur]);
+    console.table([benjamin]);
+}
+
+read();

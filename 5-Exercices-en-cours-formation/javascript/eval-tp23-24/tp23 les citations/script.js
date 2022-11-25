75084850c140abebf05e13e27eb8e02aa1299d63
Sandy Follana <follana.sandy@gmail.com>
2022-11-25 15:14:24 +0100

    trame 23

--- 5-Exercices-en-cours-formation/javascript/eval-tp23-24/tp23 les citations/script.js
+++ 5-Exercices-en-cours-formation/javascript/eval-tp23-24/tp23 les citations/script.js
@@ -1,4 +1,4 @@
-let arrayOfQuotes = [["Mamamia", "Mario"],
+let citations = [["Mamamia", "Mario"],
 ["Le coup le plus rusé que le diable ait jamais réussi, c’est de faire croire à tout le monde qu’il n’existait pas.", "Kevin Spacey - Usual Suspects de Bryan Singer"],
 ["J’ai dégusté son foie avec des fèves au beurre et un excellent Chianti", "Anthony Hopkins - Le silence des agneaux de Jonathan Demme"],
 ["La vérité c’est comme une couverture trop petite. Tu peux tirer dessus de tous les côtés, tu auras toujours les pieds froids.", "Peter Weir - Le cercle des poètes disparus"],
@@ -8,3 +8,30 @@ let arrayOfQuotes = [["Mamamia", "Mario"],
 ["Aussi loin que je me souvienne, j’ai toujours voulu être un gangster. ", "Les affranchis, Henry Hill. "],
 ["- Excusez-moi mais vous êtes en train d’uriner sur ma voiture.  -Hein ? Ho, Ha oui ! … Mais c’est parce que j’ai la même là bas, j’ai confondu ! … Je peux finir ? - Oui.", "Les bronzés font du ski. "]];
 
+
+
+var buttonSelector = document.querySelector("button");
+var pselector = document.querySelector("p");
+
+var previousCitation = null
+var actualCitation = null
+
+buttonSelector.addEventListener("click", function (event) {
+    var tamp = previousCitation 
+        
+    do previousCitation = foundCitation()
+    while(tamp == previousCitation)
+    
+    displayCitation(previousCitation)
+});
+
+
+function displayCitation(actualCitation) {
+  
+  pselector.textContent = `${citations[actualCitation][0]} | ${citations[actualCitation][1]}`;
+}
+
+function foundCitation() {
+    random = Math.floor(Math.random() * citations.length);
+    return random
+}

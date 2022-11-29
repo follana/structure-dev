98e797d5805a9c22458691fff994352a2dc105f2
Sandy follana <follana.sandy@gmail.com>
2022-11-29 15:23:36 +0100

    menu

--- 5-Exercices-en-cours-formation/javascript/burger-menu/burger.js
+++ 5-Exercices-en-cours-formation/javascript/burger-menu/burger.js
@@ -1,1 +1,13 @@
-let
+let burgerSelector= document.querySelector(".burgerStyle");
+let blockItemsSelector= document.querySelector("#menu");
+
+let itemsSolo= document.querySelector("a");
+
+
+burgerSelector.addEventListener("click", function(){
+
+})
+
+function revealMenu(){
+    
+}

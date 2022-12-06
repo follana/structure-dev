e138f5fbf5b17a27a09f5908b0c084d3b2b38145
Sandy follana <follana.sandy@gmail.com>
2022-12-06 15:07:29 +0100

    remove

--- 5-Exercices-en-cours-formation/javascript/mise en page 2/script.js
+++ 5-Exercices-en-cours-formation/javascript/mise en page 2/script.js
@@ -3,41 +3,57 @@ let p =document.querySelector("p");
 let div= document.querySelector("div");
 
 buttonSelector.forEach(button =>{
-    console.log(button)
+    console.log(button);
     button.addEventListener("click",function(){
-        if(div==0){
-            if(button.innerText=="Html"){
-                Content("p", "Html", div);    
-            }
-            else if (button.innerText =="Css"){
-                Content("p", "Css", div);    
-            }
-            else if(button.innerText =="Js"){
-                Content("p", "Js", div);   
-            }
-            else{
-                Content("p", "Html", div);
-                Content("p", "Css", div);
-                Content("p", "Js", div);
-                
-            }
+        if(button.id=="Html"){
+            Content("p", "Html", div,"page"); 
+            let html= document.getElementById("Html");
+            html.id ="htmlOut";  
         }
-        else{
-            
+        else if (button.id =="Css"){
+            Content("p", "Css", div,"déco"); 
+            let css= document.getElementById("Css"); 
+            css.id= "cssOut"  
         }
-
-    })
+        else if(button.id =="Js"){
+            Content("p", "Js", div,"anim");
+            let js = document.getElementById("Js");
+            js.id= "jsOut";  
+        }
+        else if(button.id =="Tous"){
+            Content("p", "Html", div,"page");
+            Content("p", "Css", div,"déco");
+            Content("p", "Js", div,"anim");  
+        }  
+        
+        else if (button.id == "htmlOut") {
+            console.log("removed");
+            const htmlSelector = document.getElementById("page");
+            htmlSelector.remove();
+            let htmlOut = document.getElementById("htmlOut");
+            htmlOut.id = "html";
+        } else if (button.id == "cssOut") {
+            console.log("removed");
+            const htmlSelector = document.getElementById("déco");
+            htmlSelector.remove();
+            let htmlOut = document.getElementById("cssOut");
+            cssOut.id = "css";
+        } else if (button.id == "jsOut") {
+            console.log("removed");
+            const htmlSelector = document.getElementById("anim");
+            htmlSelector.remove();
+            let htmlOut = document.getElementById("jsOut");
+            jsRem.id = "js";
+        }
+    });   
 })
 
-function Content(element_name, text, parent){
+function Content(element_name, text, parent,ide){
     const blabla =document.createElement(element_name);
     blabla.textContent =text;
     parent.appendChild(blabla);
     
 }
 
-function Eraser(){
-    
-}
 
 

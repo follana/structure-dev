cf2bc59df7d31e51aec29401dafc476ca73c27f3
Sandy follana <follana.sandy@gmail.com>
2022-12-06 18:00:31 +0100

    chronometre

--- 5-Exercices-en-cours-formation/javascript/chrono/script.js
+++ 5-Exercices-en-cours-formation/javascript/chrono/script.js
@@ -1,20 +1,75 @@
-let temps = 00
-const timerElement = document.getElementById("timer")
+let chrono = document.getElementById("chrono");
+let resetBtn = document.getElementById("reset");
+let stopBtn = document.getElementById("stop");
+let startBtn = document.getElementById("start");
 
-function chrono() {
-    let minutes = parseInt(temps / 60)
-    let secondes = parseInt(temps % 60)
+let heures = 0;
+let minutes = 0;
+let secondes = 0;
 
-  minutes = minutes < 10 ? "0" + minutes : minutes
-  secondes = secondes < 10 ? "0" + secondes : secondes
+let timeout;
 
-  timerElement.innerText = minutes + ":" + secondes
-  temps++ 
-}
+let clickstop = true;
 
-setInterval(chrono, 1000);
+const demarrer = () => {
+  if (clickstop) {
+    clickstop = false;
+    defilerTemps();
+  }
+};
 
+const stop = () => {
+  if (!clickstop) {
+    clickstop = true;
+    clearTimeout(timeout);
+  }
+};
 
+const defilerTemps = () => {
+  if (clickstop) return;
 
+  secondes = parseInt(secondes);
+  minutes = parseInt(minutes);
+  heures = parseInt(heures);
 
+  secondes++;
 
+  if (secondes == 60) {
+    minutes++;
+    secondes = 0;
+  }
+
+  if (minutes == 60) {
+    heures++;
+    minutes = 0;
+  }
+
+  if (secondes < 10) {
+    secondes = "0" + secondes;
+  }
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  if (heures < 10) {
+    heures = "0" + heures;
+  }
+
+  chrono.textContent =`${heures}:${minutes}:${secondes}`;
+
+  timeout = setTimeout(defilerTemps, 1000);
+};
+
+const reset = () => {
+  chrono.textContent = "00:00:00";
+  clickstop = true;
+  heures = 0;
+  minutes = 0;
+  secondes = 0;
+  clearTimeout(timeout);
+};
+
+startBtn.addEventListener("click", demarrer);
+stopBtn.addEventListener("click", stop);
+resetBtn.addEventListener("click", reset);

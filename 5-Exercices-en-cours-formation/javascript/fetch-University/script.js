3ae3a239706c60f1b5be7a9da97dc8be3b925471
Sandy follana <follana.sandy@gmail.com>
2022-12-07 18:01:57 +0100

    university

---
+++ 5-Exercices-en-cours-formation/javascript/fetch-University/script.js
@@ -0,0 +1,25 @@
+let button= document.querySelector("envoyer")
+let inputSelector = document.querySelector("input");
+let body= document.querySelector("body")
+var liste= document.createElement("ul");
+body.appendChild(liste)
+
+button.addEventListener("click", async function(){
+    liste.innerHtml= "";
+    let input= inputSelector.value;
+    answer= await fetch(`http://universities.hipolabs.com/search?${input}=`);
+    let universities =await answer.json()
+    
+    universities.forEach(school => {
+        if(){
+
+        }
+    });
+    
+
+
+
+    
+
+
+})
--- 5-Exercices-en-cours-formation/javascript/spread-to-do-list/script.js
+++ 5-Exercices-en-cours-formation/javascript/spread-to-do-list/script.js
@@ -5,23 +5,23 @@ let sectionSelector = document.querySelectorAll("section");
 envoyerButton.addEventListener("click", function () {
 
   let sectionCreation = document.createElement("section");
-  document.body.append(sectionCreation);
+    document.body.append(sectionCreation);
   let pSelector = document.createElement("p");
-  sectionCreation.append(pSelector);
+    sectionCreation.append(pSelector);
   let valeur = inputSelector.value;
-  pSelector.textContent = valeur;
+    pSelector.textContent = valeur;
 
   let validerButton = document.createElement("button");
-  sectionCreation.append(validerButton);
-  validerButton.textContent = "valider";
-  validerButton.addEventListener("click", function () {
+    sectionCreation.append(validerButton);
+    validerButton.textContent = "valider";
+    validerButton.addEventListener("click", function () {
     pSelector.style.color = "green";
   });
 
   let supprimerButton = document.createElement("button");
-  sectionCreation.append(supprimerButton);
-  supprimerButton.textContent = "supprimer";
-  supprimerButton.addEventListener("click", function () {
+    sectionCreation.append(supprimerButton);
+    supprimerButton.textContent = "supprimer";
+    supprimerButton.addEventListener("click", function () {
     pSelector.style.color = "red";
     pSelector.style.textDecoration = "line-through";
   });

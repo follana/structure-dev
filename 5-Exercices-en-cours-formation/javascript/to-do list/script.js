b2407b4cb1c608dee5527be453bd25358b86086e
Sandy follana <follana.sandy@gmail.com>
2022-12-07 13:02:27 +0100

    to do list

---
+++ 5-Exercices-en-cours-formation/javascript/to-do list/script.js
@@ -0,0 +1,87 @@
+(function() {
+	'use strict';
+	var tasker = {
+		init: function() {
+			this.cacheDom();
+			this.bindEvents();
+			this.evalTasklist();
+		},
+		cacheDom: function() {
+			this.taskInput = document.getElementById("input-task");
+			this.addBtn = document.getElementById("add-task");
+			this.tasklist = document.getElementById("tasks");
+			this.tasklistChildren = this.tasklist.children;
+			this.errorMessage = document.getElementById("error");
+		},
+		bindEvents: function() {
+			this.addBtn.onclick = this.addTask.bind(this);
+			this.taskInput.onkeypress = this.enterKey.bind(this);
+		},
+		evalTasklist: function() {
+			var i, chkBox, delBtn;
+			for (i = 0; i < this.tasklistChildren.length; i += 1) {
+ 
+				chkBox = this.tasklistChildren[i].getElementsByTagName("input")[0];
+				chkBox.onclick = this.completeTask.bind(this, this.tasklistChildren[i], chkBox);
+ 
+				delBtn = this.tasklistChildren[i].getElementsByTagName("button")[0];
+				delBtn.onclick = this.delTask.bind(this, i);
+			}
+		},
+		render: function() {
+			var taskLi, taskChkbx, taskVal, taskBtn, taskTrsh;
+			taskLi = document.createElement("li");
+			taskLi.setAttribute("class", "task");
+			taskChkbx = document.createElement("input");
+			taskChkbx.setAttribute("type", "checkbox");
+			taskVal = document.createTextNode(this.taskInput.value);
+			taskBtn = document.createElement("button");
+			taskTrsh = document.createElement("i");
+			taskTrsh.setAttribute("class", "fa fa-trash");
+			taskBtn.appendChild(taskTrsh);
+ 
+			taskLi.appendChild(taskChkbx);
+			taskLi.appendChild(taskVal);
+			taskLi.appendChild(taskBtn);
+ 
+			this.tasklist.appendChild(taskLi);
+ 
+		},
+		completeTask: function(i, chkBox) {
+			if (chkBox.checked) {
+				i.className = "task completed";
+			} else {
+				this.incompleteTask(i);
+			}
+		},
+		incompleteTask: function(i) {
+			i.className = "task";
+		},
+		enterKey: function(event) {
+			if (event.keyCode === 13 || event.which === 13) {
+				this.addTask();
+			}
+		},
+		addTask: function() {
+			var value = this.taskInput.value;
+			this.errorMessage.style.display = "none";
+ 
+			if (value === "") {
+				this.error();
+			} else {
+				this.render();
+				this.taskInput.value = "";
+				this.evalTasklist();
+			}
+		},
+		delTask: function(i) {
+			this.tasklist.children[i].remove();
+			this.evalTasklist();
+		},
+		error: function() {
+			this.errorMessage.style.display = "block";
+		}
+	};
+ 
+	tasker.init();
+}());

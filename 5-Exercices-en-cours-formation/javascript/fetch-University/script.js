7244086490cdf55197e9c4047f720a0a6392da88
Sandy follana <follana.sandy@gmail.com>
2022-12-08 10:20:44 +0100

    api

--- 5-Exercices-en-cours-formation/javascript/fetch-University/script.js
+++ 5-Exercices-en-cours-formation/javascript/fetch-University/script.js
@@ -1,26 +1,22 @@
-let button= document.querySelector("envoyer")
-let inputSelector = document.querySelector("input");
-let body= document.querySelector("body")
-var liste= document.createElement("ul");
-body.appendChild(liste)
-
-button.addEventListener("click", async function(){
-    liste.innerHtml= "";
-    let input= inputSelector.value;
-    answer= await fetch(`http://universities.hipolabs.com/search?${input}=`);
-    let universities =await answer.json()
-    console.log("data",universities)
-    
-    universities.forEach(school => {
-        // if(){
-
-        // }
-    });
-    
+let inputSelector = document.querySelector(".inputPays");
+let buttonSelector = document.querySelector("button");
 
 
+buttonSelector.addEventListener("click", function () {
+  var pays = inputSelector.value;
+  (async function univ() {
+    try {
+      let response = await fetch(`http://universities.hipolabs.com/search?country=${pays}`);
+      let universite = await response.json();
 
-    
-
-
-})
+      universite.forEach((nom) => {
+        let sectionCreation = document.createElement("section");
+        document.body.append(sectionCreation);
+        sectionCreation.textContent = nom.name;
+      });
+      console.log("data:", universite);
+    } catch (err) {
+      alert(err);
+    }
+  })();
+});

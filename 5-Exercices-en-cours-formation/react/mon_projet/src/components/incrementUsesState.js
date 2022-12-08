f3d466389d5f32ec3d73500c6b42e937a3ad43d6
Sandy follana <follana.sandy@gmail.com>
2022-12-08 13:34:01 +0100

    bug

--- 5-Exercices-en-cours-formation/react/mon_projet/src/components/incrementUsesState.js
+++ 5-Exercices-en-cours-formation/react/mon_projet/src/components/incrementUsesState.js
@@ -2,18 +2,23 @@ import React,{useState} from 'react';
 
 function Button1() {
     const [count, setCount] = useState(0);
+    const [state,setState] =useState(0);
+
   return (
     <div>
       <p>Vous avez cliqué {count} fois</p>
-      <button onClick={() => setCount(count + 1)}>
+      <button onClick={() => setCount(count + 1)}> 
         Cliquez ici pour ajouter
       </button>
+      
+
       <button onClick={() => setCount(count - 1)}>
          Cliquez ici pour enlever
       </button>
+      
     </div>
   );
-}
 
+}
 
 export default Button1;

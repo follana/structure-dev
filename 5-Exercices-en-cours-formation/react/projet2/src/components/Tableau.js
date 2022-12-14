961e7bb33a7ecd78135260329ab9057a19b6f5ab
Sandy follana <follana.sandy@gmail.com>
2022-12-14 13:20:53 +0100

    tab

--- 5-Exercices-en-cours-formation/react/projet2/src/components/Tableau.js
+++ 5-Exercices-en-cours-formation/react/projet2/src/components/Tableau.js
@@ -1,35 +1,74 @@
 import React from 'react';
 
-function Tableau(){
 
-    // let ColonneP ={'1':'A faire','2':'En cours','3':'Fini',}
+function Tableau() {
+   // let trucs = [
+   //    ["A faire", "En cours", "Terminé"],
+   //    ["A", "B", "C"],
+   //    ["D", "E", "F"],
+   // ];
+
+   // function AddItems(e)=>{
+      
+   //    let ajout=["","",""],
+   //    ajout.push("")
+   // }
+
+   var trucs= {
+      A_faire:[],
+      En_cours:[],
+      Terminé:[],
+   }
+
+   let tâches=[
+      {nom:"truc1", cat: "terminé"},
+      {nom:"truc2", cat: "En_cours"},
+      {nom:"truc3", cat: "A_faire"},
+      {nom:"truc4", cat: "En_cours"},
+      {nom:"truc5", cat: "terminé"},
+      {nom:"truc6", cat: "A_faire"},
+   ]
+
+   tâches.forEach((tâche)=>{
+      trucs[tâche.cat].push(<span>{tâche.nom}</span>)
+   });
 
 
-    
-
-    return(
-        <>
-          <table className="tableau" /*onSubmit={handleSubmit}*/>
+   return (
+      <>
+         <table className="tableau" /*onSubmit={handleSubmit}*/>
+            <thead>
+               <tr>
+                  <th>{trucs.nom}</th>
+               {/* {trucs.map((item, index) =>{
+               return<th>{item}</th>})} */}
+               </tr>
+            </thead>
             <tbody>
-            <tr class="one">
-               <td className="colonnes1" id="col1">A faire</td>
-               <td className="colonnes2" id="col2">En cours</td>
-               <td className="colonnes3"id="col3">Terminé</td>
-            </tr>
-            <tr class="two">
-               <td id="item A">A</td>
-               <td id="item B">B</td>
-               <td id="item c">C</td>
-            </tr>
-            </tbody>
-         </table> 
+               {/* {trucs.slice(1,trucs.length).map((item,index)=>{
+                  return(
+                     <tr>
+                  <td>{item[0]}</td>
+                  <td>{item[1]}</td>
+                  <td>{item[2]}</td>
+               </tr>)
+            })} */}
+               <tr>
+                  <td>{item[0]}</td>
+                  <td>{item[1]}</td>
+                  <td>{item[2]}</td>
+               </tr>
 
-        <label for="insert">Que souhaites-tu ajouter?</label>
-        <input type="text" placeholder="ecris ici..."></input>
-         <button type="submit" className="inserer">Ajouter</button>
-    
-    </>
-    )
+               <tr>
+                  <input type="text" placeholder="ajouter votre tâche"></input>
+                  <button type="submit">Ajouter</button>
+               </tr>
+
+            </tbody>
+         </table>
+
+      </>
+   )
 }
 
 export default Tableau;
